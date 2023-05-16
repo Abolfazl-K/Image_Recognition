e46f61cbd89017a4d18e8bcfb338461b58ce411a
Abolfazl <mr.abolfazl.ak2000@gmail.com>
2023-05-16 23:25:14 +0330

    next -> settings, profile, about

--- app/src/main/java/com/example/imagerecognition/MainActivity.kt
+++ app/src/main/java/com/example/imagerecognition/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.imagerecognition
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 
@@ -13,12 +17,44 @@ class MainActivity : AppCompatActivity() {
     private lateinit var btnLogout: Button
     private lateinit var userInfo: TextView
     private lateinit var user: FirebaseUser
+    private lateinit var navView: NavigationView
+    private lateinit var toggle: ActionBarDrawerToggle
+    private lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btnLogout = findViewById(R.id.btn_logout)
         userInfo= findViewById(R.id.userInfo)
         user = auth.currentUser!!
+
+        drawerLayout = findViewById(R.id.drawerLayout)
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView = findViewById(R.id.navView)
+        navView.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.profile -> {
+                    val intent = Intent(this, Profile::class.java)
+                    startActivity(intent)
+                }
+                R.id.settings -> {
+                    val intent = Intent(this, Settings::class.java)
+                    startActivity(intent)
+                }
+                R.id.about -> {
+                    val intent = Intent(this, About::class.java)
+                    startActivity(intent)
+                }
+            }
+            true
+        }
+
+
 
         userInfo.text = user.email.toString()
 
@@ -28,5 +64,12 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
