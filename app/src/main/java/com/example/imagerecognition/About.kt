ea0d172bd7f1509914ec4fe76ac3330ae7c58e6a
Abolfazl <mr.abolfazl.ak2000@gmail.com>
2023-05-17 19:25:39 +0330

    about is done next -> settings and profile

--- app/src/main/java/com/example/imagerecognition/About.kt
+++ app/src/main/java/com/example/imagerecognition/About.kt
@@ -1,11 +1,21 @@
 package com.example.imagerecognition
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class About : AppCompatActivity() {
+    private lateinit var btnBackToMain: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+
+        btnBackToMain = findViewById(R.id.btnBackToMain)
+        btnBackToMain.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
