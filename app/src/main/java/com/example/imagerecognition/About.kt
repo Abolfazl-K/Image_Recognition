cf1852fd3b709837f2c269cc1addfd9354a3fb9b
Abolfazl <mr.abolfazl.ak2000@gmail.com>
2023-05-17 19:29:30 +0330

    about is done next -> settings and profile

--- app/src/main/java/com/example/imagerecognition/About.kt
+++ app/src/main/java/com/example/imagerecognition/About.kt
@@ -1,9 +1,9 @@
 package com.example.imagerecognition
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class About : AppCompatActivity() {
     private lateinit var btnBackToMain: Button
@@ -14,6 +14,7 @@ class About : AppCompatActivity() {
         btnBackToMain = findViewById(R.id.btnBackToMain)
         btnBackToMain.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
             startActivity(intent)
             finish()
         }
