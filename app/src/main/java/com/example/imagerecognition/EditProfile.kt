26730013eb2c75a2baff4767515dc48f540ed194
Abolfazl <mr.abolfazl.ak2000@gmail.com>
2023-05-19 22:12:13 +0330

    sex spinner was added TODO -> user information's

--- app/src/main/java/com/example/imagerecognition/EditProfile.kt
+++ app/src/main/java/com/example/imagerecognition/EditProfile.kt
@@ -2,6 +2,7 @@ package com.example.imagerecognition
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.example.imagerecognition.databinding.ActivityEditProfileBinding
 
 class EditProfile : AppCompatActivity() {
@@ -10,11 +11,15 @@ class EditProfile : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         binding = ActivityEditProfileBinding.inflate(layoutInflater)
+        val genderOptions = resources.getStringArray(R.array.gender_options)
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, genderOptions)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.sexSpinner.adapter = adapter
 
         binding.saveButton.setOnClickListener {
             //TODO set on click listener for save button on edit profile!
         }
-
 
         setContentView(binding.root)
     }
